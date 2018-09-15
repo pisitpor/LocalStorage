c8b970f790354f7a0aefe0eb2d2835e10d00e408
pisitpor <pisitpor@live.com>
2018-09-15 16:29:06 +0700

    Local Storage

--- app/src/main/java/com/arit/demo/localstorage/BaseApplication.java
+++ app/src/main/java/com/arit/demo/localstorage/BaseApplication.java
@@ -3,6 +3,11 @@ package com.arit.demo.localstorage;
 import android.app.Application;
 import android.content.SharedPreferences;
 
+import com.arit.demo.localstorage.model.User;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class BaseApplication extends Application {
     @Override
     public void onCreate() {
@@ -16,5 +21,29 @@ public class BaseApplication extends Application {
         editor.putBoolean("safe-mode",true);
 
         editor.apply();
+
+        Realm.init(this);
+
+        /*RealmConfiguration config = new RealmConfiguration
+                                        .Builder(getApplicationContext())
+                                        .name("localstorage.realm")
+                                        .build();*/
+        Realm realm = Realm.getDefaultInstance();
+
+//        realm.createObject(User.class); // Managed Object
+        final User user = new User();     // Unmanaged Object
+        user.setFirstname("John");
+        user.setLastname("Doe");
+
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                realm.insert(user);
+            }
+        });
+    }
+    @Override
+    public void onTerminate(){
+        super.onTerminate();
     }
 }
--- app/src/main/java/com/arit/demo/localstorage/MainActivity.java
+++ app/src/main/java/com/arit/demo/localstorage/MainActivity.java
@@ -5,9 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.arit.demo.localstorage.model.User;
+
+import java.util.Observable;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tvShowPreference)
@@ -29,5 +36,18 @@ public class MainActivity extends AppCompatActivity {
                 .getSharedPreferences("settings",MODE_PRIVATE);
         this.displayname = pref.getString("display-name","");
         this.tvShowPreference.setText(this.displayname);
+    }
+
+    @OnClick(R.id.btnGetRealmData)
+    public void showRealmData(){
+        Realm realm = Realm.getDefaultInstance();
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                User user = realm.where(User.class).findFirst();
+
+                tvShowPreference.setText(user.getFirstname());
+            }
+        });
     }
 }
